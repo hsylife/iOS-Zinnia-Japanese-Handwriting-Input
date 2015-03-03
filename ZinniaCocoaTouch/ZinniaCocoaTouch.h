9a6101a069851ae9596c23fe2681140637ffe6ad
morten <mbertz@Richelieu.local>
2015-03-03 12:38:34 +0900

    Initial Commit

---
+++ ZinniaCocoaTouch/ZinniaCocoaTouch.h
@@ -0,0 +1,19 @@
+//
+//  ZinniaCocoaTouch.h
+//  ZinniaCocoaTouch
+//
+//  Created by Morten Bertz on 3/3/15.
+//  Copyright (c) 2015 Morten Bertz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZinniaCocoaTouch.
+FOUNDATION_EXPORT double ZinniaCocoaTouchVersionNumber;
+
+//! Project version string for ZinniaCocoaTouch.
+FOUNDATION_EXPORT const unsigned char ZinniaCocoaTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZinniaCocoaTouch/PublicHeader.h>
+
+
